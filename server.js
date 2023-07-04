521d87ead69b12d6d10dd255a6582a376f3750d6
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-04 21:38:16 +0200

    Add no express alternative in comment

--- server.js
+++ server.js
@@ -22,6 +22,33 @@ app.get('/history', (req, res) => {
   res.send('<h1>History</h1>');
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+// Similar solution without Express.js
+//
+// app.listen(8000, () => {
+//   console.log('Server is running on port: 8000');
+// });
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+
+//   if(req.url === '/' && req.method === 'GET') {
+//     res.write('<h1>My first server!</h1>');
+//     res.end();
+//   }
+//   else if(req.url === 'about/' && req.method === 'GET') {
+//     res.write('<h1>About</h1>');
+//     res.end();
+//   }
+
+//   // ... then similar other routes
+
+// })
+
+// server.listen(8000, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err);
+//   }
+
+//   console.log(`server is listening on ${8000}`);
+// });
